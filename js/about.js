49bfb47e614967350b546e9ed6fa76c61ec26e27
yoqubovabdulaziz <abdulazizdasturlash@gmail.com>
2023-12-17 23:40:02 +0500

    afhafaf

--- js/about.js
+++ js/about.js
@@ -303,3 +303,35 @@ modeBtn.addEventListener("click", function () {
   }
   document.body.classList.toggle("dark");
 });
+
+
+// NAVBAR SHRINK
+window.addEventListener("scroll", function () {
+  shrink();
+});
+
+let navbar = document.getElementById("navbar");
+
+function shrink() {
+  if (scrollY > 100) {
+    navbar.classList.add("navbar-shrink");
+  } else {
+    navbar.classList.remove("navbar-shrink");
+  }
+}
+
+// TOGGLE
+
+function openNavbar() {
+  document.getElementById("navbar-responsive").style.top = "0";
+}
+function closeNavbar() {
+  document.getElementById("navbar-responsive").style.top = "-100%";
+  document.getElementById("main").style.top = "-100%";
+  document.getElementById("hero").style.top = "-100%";
+}
+
+document.getElementById("navbar-open").addEventListener("click", openNavbar);
+document.getElementById("navbar-close").addEventListener("click", closeNavbar);
+document.getElementById("main").addEventListener("click", closeNavbar);
+document.getElementById("hero").addEventListener("click", closeNavbar);
