78b62ba06bec85181bdaca0c427adf0bf4036bc5
yoqubovabdulaziz <abdulazizdasturlash@gmail.com>
2023-12-17 23:09:10 +0500

    kljj

--- js/main.js
+++ js/main.js
@@ -11,176 +11,267 @@ setTimeout(() => {
 
 // ANIMATION
 
-// One of my first <canvas> experiments, woop! :D
+// ANIMATION
 
-var SCREEN_WIDTH = window.innerWidth;
-var SCREEN_HEIGHT = window.innerHeight;
+window.requestAnimFrame = function () {
+  return (
+    window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function (callback) {
+      window.setTimeout(callback);
+    }
+  );
+};
 
-var RADIUS = 70;
-
-var RADIUS_SCALE = 1;
-var RADIUS_SCALE_MIN = 1;
-var RADIUS_SCALE_MAX = 1.5;
-
-var QUANTITY = 25;
-
-var canvas;
-var context;
-var particles;
-
-var mouseX = SCREEN_WIDTH * 0.5;
-var mouseY = SCREEN_HEIGHT * 0.5;
-var mouseIsDown = false;
-
-function init() {
-  canvas = document.getElementById("world");
-
-  if (canvas && canvas.getContext) {
-    context = canvas.getContext("2d");
-
-    // Register event listeners
-    window.addEventListener("mousemove", documentMouseMoveHandler, false);
-    window.addEventListener("mousedown", documentMouseDownHandler, false);
-    window.addEventListener("mouseup", documentMouseUpHandler, false);
-    document.addEventListener("touchstart", documentTouchStartHandler, false);
-    document.addEventListener("touchmove", documentTouchMoveHandler, false);
-    window.addEventListener("resize", windowResizeHandler, false);
-
-    createParticles();
-
-    windowResizeHandler();
-
-    setInterval(loop, 1000 / 60);
-  }
+function init(elemid) {
+  let canvas = document.getElementById(elemid),
+    c = canvas.getContext("2d"),
+    w = (canvas.width = window.innerWidth),
+    h = (canvas.height = window.innerHeight);
+  c.fillStyle = "rgba(30,30,30,1)";
+  c.fillRect(0, 0, w, h);
+  return { c: c, canvas: canvas };
 }
 
-function createParticles() {
-  particles = [];
+window.onload = function () {
+  let c = init("canvas").c,
+    canvas = init("canvas").canvas,
+    w = (canvas.width = window.innerWidth),
+    h = (canvas.height = window.innerHeight),
+    mouse = { x: false, y: false },
+    last_mouse = {};
 
-  for (var i = 0; i < QUANTITY; i++) {
-    var particle = {
-      size: 1,
-      position: { x: mouseX, y: mouseY },
-      offset: { x: 0, y: 0 },
-      shift: { x: mouseX, y: mouseY },
-      speed: 0.01 + Math.random() * 0.04,
-      targetSize: 1,
-      fillColor: "#" + ((Math.random() * 0x404040 + 0xaaaaaa) | 0).toString(16),
-      orbit: RADIUS * 0.5 + RADIUS * 0.5 * Math.random(),
-    };
-
-    particles.push(particle);
-  }
-}
-
-function documentMouseMoveHandler(event) {
-  mouseX = event.clientX - (window.innerWidth - SCREEN_WIDTH) * 0.5;
-  mouseY = event.clientY - (window.innerHeight - SCREEN_HEIGHT) * 0.5;
-}
-
-function documentMouseDownHandler(event) {
-  mouseIsDown = false;
-}
-
-function documentMouseUpHandler(event) {
-  mouseIsDown = false;
-}
-
-function documentTouchStartHandler(event) {
-  if (event.touches.length == 1) {
-    event.preventDefault();
-
-    mouseX = event.touches[0].pageX - (window.innerWidth - SCREEN_WIDTH) * 0.5;
-    mouseY =
-      event.touches[0].pageY - (window.innerHeight - SCREEN_HEIGHT) * 0.5;
-  }
-}
-
-function documentTouchMoveHandler(event) {
-  if (event.touches.length == 1) {
-    event.preventDefault();
-
-    mouseX = event.touches[0].pageX - (window.innerWidth - SCREEN_WIDTH) * 0.5;
-    mouseY =
-      event.touches[0].pageY - (window.innerHeight - SCREEN_HEIGHT) * 0.5;
-  }
-}
-
-function windowResizeHandler() {
-  SCREEN_WIDTH = window.innerWidth;
-  SCREEN_HEIGHT = window.innerHeight;
-
-  canvas.width = SCREEN_WIDTH;
-  canvas.height = SCREEN_HEIGHT;
-}
-
-function loop() {
-  if (mouseIsDown) {
-    RADIUS_SCALE += (RADIUS_SCALE_MAX - RADIUS_SCALE) * 0.02;
-  } else {
-    RADIUS_SCALE -= (RADIUS_SCALE - RADIUS_SCALE_MIN) * 0.02;
+  function dist(p1x, p1y, p2x, p2y) {
+    return Math.sqrt(Math.pow(p2x - p1x, 2) + Math.pow(p2y - p1y, 2));
   }
 
-  RADIUS_SCALE = Math.min(RADIUS_SCALE, RADIUS_SCALE_MAX);
+  class segment {
+    constructor(parent, l, a, first) {
+      this.first = first;
+      if (first) {
+        this.pos = {
+          x: parent.x,
+          y: parent.y,
+        };
+      } else {
+        this.pos = {
+          x: parent.nextPos.x,
+          y: parent.nextPos.y,
+        };
+      }
+      this.l = l;
+      this.ang = a;
+      this.nextPos = {
+        x: this.pos.x + this.l * Math.cos(this.ang),
+        y: this.pos.y + this.l * Math.sin(this.ang),
+      };
+    }
+    update(t) {
+      this.ang = Math.atan2(t.y - this.pos.y, t.x - this.pos.x);
+      this.pos.x = t.x + this.l * Math.cos(this.ang - Math.PI);
+      this.pos.y = t.y + this.l * Math.sin(this.ang - Math.PI);
+      this.nextPos.x = this.pos.x + this.l * Math.cos(this.ang);
+      this.nextPos.y = this.pos.y + this.l * Math.sin(this.ang);
+    }
+    fallback(t) {
+      this.pos.x = t.x;
+      this.pos.y = t.y;
+      this.nextPos.x = this.pos.x + this.l * Math.cos(this.ang);
+      this.nextPos.y = this.pos.y + this.l * Math.sin(this.ang);
+    }
+    show() {
+      c.lineTo(this.nextPos.x, this.nextPos.y);
+    }
+  }
 
-  // Reduce the opacity of the background to create a trailing effect
-  context.fillStyle = "rgba(0,0,0,0.05)";
-  context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+  class tentacle {
+    constructor(x, y, l, n, a) {
+      this.x = x;
+      this.y = y;
+      this.l = l;
+      this.n = n;
+      this.t = {};
+      this.rand = Math.random();
+      this.segments = [new segment(this, this.l / this.n, 0, true)];
+      for (let i = 1; i < this.n; i++) {
+        this.segments.push(
+          new segment(this.segments[i - 1], this.l / this.n, 0, false)
+        );
+      }
+    }
+    move(last_target, target) {
+      this.angle = Math.atan2(target.y - this.y, target.x - this.x);
+      this.dt = dist(last_target.x, last_target.y, target.x, target.y) + 5;
+      this.t = {
+        x: target.x - 0.8 * this.dt * Math.cos(this.angle),
+        y: target.y - 0.8 * this.dt * Math.sin(this.angle),
+      };
+      if (this.t.x) {
+        this.segments[this.n - 1].update(this.t);
+      } else {
+        this.segments[this.n - 1].update(target);
+      }
+      for (let i = this.n - 2; i >= 0; i--) {
+        this.segments[i].update(this.segments[i + 1].pos);
+      }
+      if (
+        dist(this.x, this.y, target.x, target.y) <=
+        this.l + dist(last_target.x, last_target.y, target.x, target.y)
+      ) {
+        this.segments[0].fallback({ x: this.x, y: this.y });
+        for (let i = 1; i < this.n; i++) {
+          this.segments[i].fallback(this.segments[i - 1].nextPos);
+        }
+      }
+    }
+    show(target) {
+      if (dist(this.x, this.y, target.x, target.y) <= this.l) {
+        c.globalCompositeOperation = "lighter";
+        c.beginPath();
+        c.lineTo(this.x, this.y);
+        for (let i = 0; i < this.n; i++) {
+          this.segments[i].show();
+        }
+        c.strokeStyle =
+          "hsl(" +
+          (this.rand * 60 + 180) +
+          ",100%," +
+          (this.rand * 60 + 25) +
+          "%)";
+        c.lineWidth = this.rand * 2;
+        c.lineCap = "round";
+        c.lineJoin = "round";
+        c.stroke();
+        c.globalCompositeOperation = "source-over";
+      }
+    }
+    show2(target) {
+      c.beginPath();
+      if (dist(this.x, this.y, target.x, target.y) <= this.l) {
+        c.arc(this.x, this.y, 2 * this.rand + 1, 0, 2 * Math.PI);
+        c.fillStyle = "white";
+      } else {
+        c.arc(this.x, this.y, this.rand * 2, 0, 2 * Math.PI);
+        c.fillStyle = "darkcyan";
+      }
+      c.fill();
+    }
+  }
 
-  for (i = 0, len = particles.length; i < len; i++) {
-    var particle = particles[i];
+  let maxl = 300,
+    minl = 50,
+    n = 30,
+    numt = 500,
+    tent = [],
+    clicked = false,
+    target = { x: 0, y: 0 },
+    last_target = {},
+    t = 0,
+    q = 10;
 
-    var lp = { x: particle.position.x, y: particle.position.y };
-
-    // Rotation
-    particle.offset.x += particle.speed;
-    particle.offset.y += particle.speed;
-
-    // Follow mouse with some lag
-    particle.shift.x += (mouseX - particle.shift.x) * particle.speed;
-    particle.shift.y += (mouseY - particle.shift.y) * particle.speed;
-
-    // Apply position
-    particle.position.x =
-      particle.shift.x +
-      Math.cos(i + particle.offset.x) * (particle.orbit * RADIUS_SCALE);
-    particle.position.y =
-      particle.shift.y +
-      Math.sin(i + particle.offset.y) * (particle.orbit * RADIUS_SCALE);
-
-    // Limit to screen bounds
-    particle.position.x = Math.max(
-      Math.min(particle.position.x, SCREEN_WIDTH),
-      0
+  for (let i = 0; i < numt; i++) {
+    tent.push(
+      new tentacle(
+        Math.random() * w,
+        Math.random() * h,
+        Math.random() * (maxl - minl) + minl,
+        n,
+        Math.random() * 2 * Math.PI
+      )
     );
-    particle.position.y = Math.max(
-      Math.min(particle.position.y, SCREEN_HEIGHT),
-      0
-    );
-
-    particle.size += (particle.targetSize - particle.size) * 0.05;
-
-    if (Math.round(particle.size) == Math.round(particle.targetSize)) {
-      particle.targetSize = 1 + Math.random() * 7;
+  }
+  function draw() {
+    if (mouse.x) {
+      target.errx = mouse.x - target.x;
+      target.erry = mouse.y - target.y;
+    } else {
+      target.errx =
+        w / 2 +
+        ((h / 2 - q) * Math.sqrt(2) * Math.cos(t)) /
+          (Math.pow(Math.sin(t), 2) + 1) -
+        target.x;
+      target.erry =
+        h / 2 +
+        ((h / 2 - q) * Math.sqrt(2) * Math.cos(t) * Math.sin(t)) /
+          (Math.pow(Math.sin(t), 2) + 1) -
+        target.y;
     }
 
-    context.beginPath();
-    context.fillStyle = particle.fillColor;
-    context.strokeStyle = particle.fillColor;
-    context.lineWidth = particle.size;
-    context.moveTo(lp.x, lp.y);
-    context.lineTo(particle.position.x, particle.position.y);
-    context.stroke();
-    context.arc(
-      particle.position.x,
-      particle.position.y,
-      particle.size / 2,
-      0,
-      Math.PI * 2,
-      true
-    );
-    context.fill();
-  }
-}
+    target.x += target.errx / 10;
+    target.y += target.erry / 10;
 
-window.onload = init;
+    t += 0.01;
+
+    c.beginPath();
+    c.arc(
+      target.x,
+      target.y,
+      dist(last_target.x, last_target.y, target.x, target.y) + 5,
+      0,
+      2 * Math.PI
+    );
+    c.fillStyle = "hsl(210,100%,80%)";
+    c.fill();
+
+    for (i = 0; i < numt; i++) {
+      tent[i].move(last_target, target);
+      tent[i].show2(target);
+    }
+    for (i = 0; i < numt; i++) {
+      tent[i].show(target);
+    }
+    last_target.x = target.x;
+    last_target.y = target.y;
+  }
+
+  canvas.addEventListener(
+    "mousemove",
+    function (e) {
+      last_mouse.x = mouse.x;
+      last_mouse.y = mouse.y;
+
+      mouse.x = e.pageX - this.offsetLeft;
+      mouse.y = e.pageY - this.offsetTop;
+    },
+    false
+  );
+
+  canvas.addEventListener("mouseleave", function (e) {
+    mouse.x = false;
+    mouse.y = false;
+  });
+
+  canvas.addEventListener(
+    "mousedown",
+    function (e) {
+      clicked = true;
+    },
+    false
+  );
+
+  canvas.addEventListener(
+    "mouseup",
+    function (e) {
+      clicked = false;
+    },
+    false
+  );
+
+  function loop() {
+    window.requestAnimFrame(loop);
+    c.clearRect(0, 0, w, h);
+    draw();
+  }
+
+  window.addEventListener("resize", function () {
+    (w = canvas.width = window.innerWidth),
+      (h = canvas.height = window.innerHeight);
+    loop();
+  });
+
+  loop();
+  setInterval(loop, 1000 / 60);
+};
